52134da18ee508f262db7f7a3dcc2d00d6e1255b
Abdullahi <Guure1440@gmail.com>
2025-07-08 23:09:01 +0300

    importing skills  and project sections

--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import Header from './Hero'
-
+import { useSpring, animated } from '@react-spring/web'
+import Skills from '../Skills'
+import Project from '../Project'
 const Home = () => {
+  const style=useSpring({opacity:1, from:{opacity:0}});
   return (
     <div>
 
       <Header />
+      <Skills/>
+      <Project/>
+      {/* <motion.div
+      initial={{opacity:0, y:-50}}
+      animate={{opacity:1, y:0}}
+      transition={{duration:0.8}}
+      >
+        <h3 className='text-center text-6xl text-purple-600 uppercase'>I am ready to enhance you </h3>
+
+      </motion.div> */}
+      <animated.div style={style}> <h3 className='text-center text-6xl text-purple-600 uppercase'>I am ready to enhance you </h3>
+
+
+      </animated.div>
+
+
     </div>
   )
 }
