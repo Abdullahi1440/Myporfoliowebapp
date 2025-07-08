a65456c01eb82e8d30a8703c0bc7952609d4dd20
Abdullahi <Guure1440@gmail.com>
2025-07-08 23:04:46 +0300

    styling blog page

--- src/Component/Pages/Blog.jsx
+++ src/Component/Pages/Blog.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
 
+
+  // import logo from ".../assets/logo.png";
 const Blog = () => {
   return (
-    <div className='flex justify-center text-3xl text-green-500 uppercase'>Welcome to Blog</div>
+    <div className='flex flex-col justify-center  items-center mx-auto mt-2  py-5 px-5 w-full container '>
+      <div className='block mb-5 '>
+      <h2 className='text-center items-center font-bold text-5xl text-purple-400'>My Journey sofware developer </h2>
+
+      </div>
+
+      
+      <div className="flex   flex-wrap mx-auto justify-center items-center w-full ">
+        <img src="" alt="" className='w-50 object-cover h-50' />
+        <p className=' flex text-center font-light text-2xl mt-3 '>My journey into software development began during my university years, where I majored in Computer Science. What started as curiosity quickly grew into a full-blown passion as I began building small web apps and contributing to open-source projects. Since then, I've worked on a variety of freelance and team-based projects, refining my skills in JavaScript, React, and backend technologies like Node.js and Express.
+
+        </p>
+      </div>
+  
+    </div>
   )
 }
 
