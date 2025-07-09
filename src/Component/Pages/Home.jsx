e00c6d5585cc4df469333e99830344f496af1a2a
Abdullahi <Guure1440@gmail.com>
2025-07-09 07:31:22 +0300

    enhance text and modified typo

--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -20,7 +20,9 @@ const Home = () => {
         <h3 className='text-center text-6xl text-purple-600 uppercase'>I am ready to enhance you </h3>
 
       </motion.div> */}
-      <animated.div style={style}> <h3 className='text-center text-6xl text-purple-600 uppercase'>I am ready to enhance you </h3>
+      <animated.div style={style}> <h3 className='text-center text-4xl text-purple-600  capitalize'>
+        A business has not got a website, which is like a business without dreaming and goals. I advise you to improve using technology to achieve your goals with your business.
+         </h3>
 
 
       </animated.div>
