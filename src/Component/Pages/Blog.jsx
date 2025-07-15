25f7bc1cf3962c73ffbdd2877adaf0bfa021f2ab
Abdullahi <Guure1440@gmail.com>
2025-07-15 16:41:50 +0300

    Adding image blog

--- src/Component/Pages/Blog.jsx
+++ src/Component/Pages/Blog.jsx
@@ -1,5 +1,5 @@
 
-
+// import myimage from ".../assets/";
   // import logo from ".../assets/logo.png";
 const Blog = () => {
   return (
@@ -11,8 +11,13 @@ const Blog = () => {
 
       
       <div className="flex   flex-wrap mx-auto justify-center items-center w-full ">
-        <img src="" alt="" className='w-50 object-cover h-50' />
-        <p className=' flex text-center font-light text-2xl mt-3 '>My journey into software development began during my university years, where I majored in Computer Science. What started as curiosity quickly grew into a full-blown passion as I began building small web apps and contributing to open-source projects. Since then, I've worked on a variety of freelance and team-based projects, refining my skills in JavaScript, React, and backend technologies like Node.js and Express.
+        <img src="myimage.jpg" alt="" className='w-60 object-cover h-60 rounded-2xl' />
+        <p className=' flex text-center font-light text-2xl mt-4 '>
+          My journey into software development began during my  University Of Bosaso (UOB) years.
+
+          where I majored in computer science. What started as curiosity quickly grew into a full-blown passion as I began building small web apps and contributing to open-source projects.
+
+          Since then, I've worked on various freelance and team-based projects, refining my skills in JavaScript, React, and backend technologies like Node.js and Express. Along the way, I discovered the importance of collaboration and continuous learning, which have become integral to my growth as a developer. Each project has presented new challenges, allowing me to adapt and innovate while also deepening my understanding of user experience and software architecture.
 
         </p>
       </div>
